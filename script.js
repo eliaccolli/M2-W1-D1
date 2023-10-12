f54450d9e1ddc6069330b17c71c700d1c14622da
Elia Accolli <elia.accolli@outlook.it>
2023-10-12 17:33:49 +0200

    Create script.js
    rev 1.0

---
+++ script.js
@@ -0,0 +1,9 @@
+let risposta1;
+risposta1 = "sequenza di istruzioni e operazioni che servono per risolvere un determinato problema";
+let risposta2;
+risposta2 = "è un contenitore, inizialmente vuoto, che può contenere diversi tipi di valori (stringhe, numeri, ecc..)";
+let risposta3;
+risposta3 = "underfined indica una variabile alla quale non è stata nessun valore, null indica una variabile già dichiarata ma con valore sconosciuto"
+console.log("Risposta alla domanda 1: " + risposta1);
+console.log("Risposta alla domanda 2: " + risposta2);
+console.log("Risposta alla domanda 3: " + risposta3);
